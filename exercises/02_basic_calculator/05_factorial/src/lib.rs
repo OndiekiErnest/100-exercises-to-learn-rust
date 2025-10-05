0a8f8fff70b6e67051a85505e61dbdcb779c3c7f
Ernesto2018 <ernestondieki12@gmail.com>
2025-10-05 13:20:25 +0300

    Add the factorial function

--- exercises/02_basic_calculator/05_factorial/src/lib.rs
+++ exercises/02_basic_calculator/05_factorial/src/lib.rs
@@ -10,6 +10,14 @@
 //
 // Use only what you learned! No loops yet, so you'll have to use recursion!
 
+fn factorial(number: u64) -> u64 {
+    if number == 0 || number == 1 {
+        1
+    } else {
+        number * factorial(number - 1)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::factorial;
