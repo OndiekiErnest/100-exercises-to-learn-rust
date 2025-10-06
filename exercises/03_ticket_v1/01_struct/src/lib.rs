5b9d986b5e3e6c0ba0709b5dc0497bdffedc5d86
Ernesto2018 <ernestondieki12@gmail.com>
2025-10-06 11:39:02 +0300

    Add struct with 1 implementation

--- exercises/03_ticket_v1/01_struct/src/lib.rs
+++ exercises/03_ticket_v1/01_struct/src/lib.rs
@@ -2,8 +2,21 @@
 // - `price`, an unsigned integer
 // - `quantity`, an unsigned integer
 //
+struct Order {
+    price: u32,
+    quantity: u32,
+}
 // It should also have a method named `is_available` that returns a `true` if the quantity is
 // greater than 0, otherwise `false`.
+impl Order {
+    fn is_available(&self) -> bool {
+        if self.quantity > 0 {
+            true
+        } else {
+            false
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
